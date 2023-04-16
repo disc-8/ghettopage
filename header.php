ca2468f2cea3d6ea2e230f5d81a3e1e358c73ba3
snuskgubbe <130178512+snuskgubbe@users.noreply.github.com>
2023-04-16 13:45:11 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,114 @@
+<?php
+ $color="yellow"
+ $site-title="DiSC-8 Ghettopage Example"
+?>
+
+<!--
+             . '..:'.                  "
+               ..:'/|  PROGRAMMAS PRIEKš STIPRAJIEM // PROGRAMY DLA POTĘżNYCH
+             '..:'/&| --------------------------------------------------------
+          . '..:'/&&|         :&&###&& "        :&####&&
+            ..:'/&|&|        :&# :: :#&"       :&#:   :#&
+       .  '..:'/&/|&|        &# :  ..:#"    . :&#:     :#&       :8888888:     /
+       . '..:'/&/:|&|       :&#:   .. :"&  .. :&#:      &&      :88 ::: 88:   /
+         ..:'/&/:.|&|       :&#:    ..."#& .. :&#:      #:     :88 ::::: 88: /
+       '..:'/&/:::|&|   /   :&#      ..":&  ..:&#::    /:     :88  ####   8:/
+    . '..:'/&/:.:.|&| .8o/   :&#:     ."i:  .. :&#:   ,       :8 ::::::: 8:/
+      ..:'/&/:::. |&| _(@_    :&#::    "  : .. :&#:           :8 :::::: 8:/
+    '..:'/&/:.:...|&| /i'^     :&&##:_-"---  . :&#::    _____.,:88888888_:,.____
+ . '..:'/&/:::  . |&|  /  _     ::&&###"##&&  . :&#:         :88 #### 88:
+   ..:'/&/:.:... .|&|__r .&]       .^^^"^^i#& . :&#::       :88 #####  8:
+ '..:'/&/@)::@).. |&|  ^^:#&]    :  ...".  i#& . :&#:       :88 :::::  8:
+ ..:'/&/:.::.. .  |&|     :#&]   ::    "... i#&  :&#:        :88 :::::8:
+..:'/&/::::....   |&|      :#&]  .#:      .. |#& :&#::        :8888888:
+.:'/&/:.::.. .  . |&|       :#&] .&#:      . |#&  :&#:            /
+  /&/-------------|&|__      :#&] &#:: "   . |#&  :&#::          /
+&&&&&&&&&&&&&&&&&&&&&&&|      :#&] &#::"   . |#& . :&#::     #& /
+&|^^^^^^^^^^^^^^^^^^^|&|     ./:#&] &#:"    /#&  .. :&#::   #& /
+^"                   "^'    .,r:&]/  &&"####&#    .. :&&###&& /
+                                      `"^^^''          `^^^'
+              u shudnt b hier afrikaboy
+              if u DARE 2ry 2 2k sumfyn 4rom DYSK8
+              well bby we gon dare 2 take sumfin 4rom U!
+              spesifikally:your anus virginitiya
+              prepare for DICK8,specifically 8 DIX!
+              yeah bby i been 2 chornobyl dey just grew on me
+-->
+  <style>
+    html,body{
+      background-color:black/*das raysiz*/;
+      color:white;
+      font-family:serif;
+      margin:0;
+      padding:0;
+      background-image: linear-gradient(31deg, #0a0a0af0 25%, transparent 25%), linear-gradient(-31deg, #0b0b0b 25%, transparent 25%), linear-gradient(31deg, transparent 75%, #0a0a0a 75%), linear-gradient(-31deg, transparent 75%, #0b0b0bf0 75%);
+      background-size: 20px 20px;
+      background-position: 0 0, 0 10px, 10px -10px, -10px 0px;
+    }
+    h1{
+      color:<?php echo "$color" ?>;
+      font-weight:bold;
+      padding:0;
+      margin:0;
+      font-style: italic;
+    }
+    div{
+        border:5px <?php echo $color ?> ridge;
+    }
+    pre,td{
+      background-color:rgba(3,3,3,0.8);
+      background-image:repeating-linear-gradient(rgb(14,14,14) 0px,rgb(20,20,28),rgb(14,14,14) 6px);
+      padding:2px;
+      white-space: pre-wrap;
+      border:2px ridge gray;
+      margin:5px;
+    }
+    pre:before{content:" 8=D ";font-size:larger;color:grey}
+    p{margin:5px;}
+    .censor{color:transparent;background-color:black;width:fit-content;}
+    .censor:hover{color:white}
+    h1::before{content:'// ';font-weight:bold;color:yellowgreen;margin-left:5px;}
+    .rumNbetsy{
+        margin-right:200px;
+        width:205px;
+        max-width:205px;
+        position:fixed;
+        height:100%;
+        filter: brightness(0.4);
+        background: linear-gradient(to top, <?php echo "$color" ?> 0%, rgba(0,0,0,0) 100%);
+        float:left;
+    }
+    .ulookynsexcymama{
+        margin-left:215px;
+        background: linear-gradient( rgba(88,88,0,1) 0%, rgba(0,0,0,0) 100%);
+    }
+    a{color:white;text-decoration:underline;}
+    a::before{content:"#";color:grey;font-size:5mm;text-decoration:none;}
+    .nordeste{
+        top:90%;
+        font-size:x-small;
+        width:70%;
+        text-align:center;
+        margin-left:auto;
+        margin-right:5px;
+
+    }
+  </style>
+  <meta charset="utf-8">
+  <title><?php echo "$page-title - $site-name" ?></title>
+<html>
+<div><h1><span style="color:white"><?php echo "$site-name" ?></span><span style="color:grey"> // </span><?php echo "$page-title" ?></div>
+<div class="rumNbetsy">
+    <p>
+      <h1>PartnerWebsites寄存侏儒色情制品的网站</h1>
+    <a href="http://example.org">one website</a><br>
+    <a href="https://github.com/disc-8">github/pornhub</a>
+    </p>
+    <hr>
+    <p>
+      <h1>NAVIGATION/навигация сайта</h1>
+      <a href="/1.php">page 1</a><br>
+      <a href="/2.php">products</a><br>
+      <a href="/3.php">example</a>
+</div>
+<div class="ulookynsexcymama">
