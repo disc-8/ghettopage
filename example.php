b43d8088601341f5123a4338ef979ed6c5e5ad2c
snuskgubbe <130178512+snuskgubbe@users.noreply.github.com>
2023-04-16 13:51:00 +0200

    Create example.php

---
+++ example.php
@@ -0,0 +1,28 @@
+<?php
+ $page-title="Home"
+ include '/header.php';
+?>
+<h1 id="welt">ХУЯ ПОСОСІ.</h1>
+DiSC-8互联网上的强奸犯群体
+你多久会对侏儒摔跤打枪对我们来说这是个日常的仪式…
+<p>hent oss en smörgås nigga, eller du DÖR! yo-ho hello der!!wellkom 2 da PL side ov da DiSC-8 rapework, we myk GIGANTIK software but not as big as my giganDICK;) we got a pleazant team of 3 3rd world nigguz 4rom 3 different xountries n we mastaz @ it!(but not ov b8tn,that title belongz 2 3 certain individuals),bby u full ov suprizes!look inside ur stereos EYES…………good EVENING!dont aktlyka snobbymodel u just won da LOTTO!</p>
+<h1 id="kto">ładashit</h1>
+<p>this is an example syt made with GhettoPAGE. suck on it nigga</p>
+<table>
+<tr><td><img src="http://xmidget.net/pfp/sveiks.png" width="48px"> <b>Lohi Inc.</b></td>
+  <td>Diz is da department 4 graфfikal gaymz.
+It has 2rue beauty art. LATVIAN SEAL OF QUALITY.</td></tr>
+<tr><td><img src="http://xmidget.net/pfp/sveiks.png" width="48px"> <b>Pus-Lohs</b></td>
+  <td>Souperyor polish technology: deez gaymz will run on ANYFFIN
+grandmas pacemaker?u know it!graffix r simpyl ANSI art, VERY liteweit.</td></tr>
+</table>
+    <pre>ghetto boy @ saldejums.tk: negao ao picona…
+test
+am ORGASMING right now.
+      </pre>
+
+</three-way></pre<!--cum--> >
+</X></p>p
+<?php
+ include '/footer.php';
+?>
