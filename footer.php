7bcda698cc6ecde6e479f72f7f4c6cfd8398b689
snuskgubbe <130178512+snuskgubbe@users.noreply.github.com>
2023-04-16 13:45:32 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,5 @@
+<p class="nordeste">
+khoppieryt © u c dere'z cevral diffrnt lvl'z 2 debil whorshippin: whoreheads, semensakrificez khanniballising balls, khandlsz&sexorcizms, animals havinsexwitdem mhammels & rabbi's but u kno i khiqqd da habyt: i just r8p u 2 deaff wiff cock semen dat eatsthrudaflesch & i dond even let u suckonit unless your mouff loox fresch i got a cock in my pocket i'm lyin i got 2 cox in my povket & bby u just dyin suckyt.. i'm ready4sex u may not b but itdontmatta its r8pmanifestindaflesch i got cock&balls & what i do is go raw yeah yu met me b4! i juss hadakondomb4! i'll stepinyouranus, lay your headonda floor spread your legs open & pissondawall.. kill ju & get a new whore!
+<br><iframe src="http://xmidget.com" />
+</p>
+</div>
